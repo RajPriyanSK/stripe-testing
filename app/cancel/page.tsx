dcb5ec1ebeb700bea6932e080c83f3fedb76a492
Raj-Priyan-SK <rajsk.ssk@gmail.com>
2026-07-15 07:20:22 +0000

    Redirect canceled checkout back home

--- app/cancel/page.tsx
+++ app/cancel/page.tsx
@@ -1,3 +1,14 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 export default function CancelPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/");
+  }, [router]);
+
   return <p>Payment cancelled.</p>;
 }
